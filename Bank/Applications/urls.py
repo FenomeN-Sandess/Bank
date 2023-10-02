9fb9176f0da0b5d9319948619eda770126d779a5
Вадим <vadfacego@gmail.com>
2023-10-03 00:30:55 +0300

    html-css

--- Bank/Applications/urls.py
+++ Bank/Applications/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
-
+    path("", views.index, name="index"),
+    path("reg/", views.reg_form, name="reg_form")
 ]
--- Bank/Applications/views.py
+++ Bank/Applications/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "index.html")
+
+def reg_form(request):
+    return render(request, "reg_form.html")
