eaa83c4102f7db950803a602dedaf69fe09ba0e9
sayfdor <mrdesper.mi@gmail.com>
2023-12-16 17:03:26 +0300

    login + choice page

--- Bank/Applications/urls.py
+++ Bank/Applications/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ready
     path("", views.index, name="index"),
     path("reg/", views.register, name="reg_form"),
     path("reg_profile/", views.registerProfile, name="reg_form_profile"),
+    # ready
     path("logout/", views.log, name="logout"),
     path("login/", views.login_view, name="login_form"),
     path("about/", views.about, name="about_form"),
